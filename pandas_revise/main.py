dd53d0b995efbc141e165cb01886bc9220429dfd
Yatin Jain <192596591+Yatin-Code@users.noreply.github.com>
2025-12-19 11:33:04 +0530

    done some work

--- pandas_revise/main.py
+++ pandas_revise/main.py
@@ -2,7 +2,6 @@
 # import csv
 # with open("002 weather-data.csv") as file:
 #     data = csv.reader(file)
-#     temp =[]
 #     for row in data:
 #         try:
 #             temperature = (int(row[1]))
@@ -11,3 +10,36 @@
 #             continue
 # print(temp)
 import pandas
+
+data = pandas.read_csv("002 weather-data.csv")
+# data_list = data["temp"].to_list()
+# print(data_list)
+
+## FINDING mean:
+
+# METHOD 1
+
+
+# total_sum = 0
+# total_num = 0
+# for i in data_list:
+#     total_num += 1
+#     total_sum +=  i
+
+
+# avg = total_sum/total_num
+# print(round(avg))
+
+# Method 2
+
+# total_sum = sum(data_list)
+# total_num = len(data_list)
+# print(total_sum/total_num)
+
+# Method 3
+
+# print(data["temp"].max())
+
+# print((data[data.temp == data.temp.max()]))
+monday = data[data.day == "Monday"]
+print(int(monday.temp) * 9/5 + 32)
