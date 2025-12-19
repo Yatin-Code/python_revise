920361865c8f9493253363e86fc9dae2cf5782c3
Yatin Jain <192596591+Yatin-Code@users.noreply.github.com>
2025-12-19 10:13:33 +0530

    add gitignore

---
+++ pandas_revise/main.py
@@ -0,0 +1,13 @@
+
+# import csv
+# with open("002 weather-data.csv") as file:
+#     data = csv.reader(file)
+#     temp =[]
+#     for row in data:
+#         try:
+#             temperature = (int(row[1]))
+#             temp.append(temperature)
+#         except ValueError:
+#             continue
+# print(temp)
+import pandas
